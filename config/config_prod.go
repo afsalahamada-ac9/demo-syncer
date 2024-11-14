24285c57d98b085c70ec04602936906808b40e49
Sudhagar Chinnaswamy <sudhagar@abovecloud9.ai>
2024-11-14 23:23:33 +0530

    add copyright to all .go files

--- config/config_prod.go
+++ config/config_prod.go
@@ -1,5 +1,11 @@
 //go:build prod
 
+/*
+ * Copyright 2024 AboveCloud9.AI Products and Services Private Limited
+ * All rights reserved.
+ * This code may not be used, copied, modified, or distributed without explicit permission.
+ */
+
 package config
 
 const (
--- usecase/label/service.go
+++ usecase/label/service.go
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2024 AboveCloud9.AI Products and Services Private Limited
+ * All rights reserved.
+ * This code may not be used, copied, modified, or distributed without explicit permission.
+ */
+
 package label
 
 import (
